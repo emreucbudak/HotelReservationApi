874f2ec4741804dc2ce8a56ed3515d1067257554
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2025-09-10 01:07:55 +0300

    ENTİTYLER EKLENDİ

---
+++ Core/HotelReservationApi.Domain/Entities/Appointments.cs
@@ -0,0 +1,19 @@
+﻿using HotelReservationApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Domain.Entities
+{
+    public class Appointments : BaseEntity
+    {
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int HowManyDays { get; set; } 
+        public int RoomsId { get; set; }
+        public Rooms Rooms { get; set; }
+    }
+}
