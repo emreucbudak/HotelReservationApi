2e66744bb45a90769918144676357217cf235f56
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2025-09-12 00:37:55 +0300

    yenı modeller ve  modellere klemeler yaptım

---
+++ Core/HotelReservationApi.Domain/Entities/HotelImages.cs
@@ -0,0 +1,17 @@
+﻿using HotelReservationApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Domain.Entities
+{
+    public class HotelImages : BaseEntity
+    {
+        public string ImageUrl { get; set; }
+        public int HotelId { get; set; }
+        public Hotels Hotel { get; set; }
+        public string ImageTitle { get; set; }
+    }
+}
---
+++ Core/HotelReservationApi.Domain/Entities/HowFarSpecialPlace.cs
@@ -0,0 +1,17 @@
+﻿using HotelReservationApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Domain.Entities
+{
+    public class HowFarSpecialPlace : BaseEntity
+    {
+        public int Id   { get; set; }
+        public string PlaceName { get; set; }
+        public int SpecialPlaceCategoryId { get; set; }
+        public SpecialPlaceCategory SpecialPlaceCategory { get; set; }
+    }
+}
