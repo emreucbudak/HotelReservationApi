8ce9556a24a158f1b174bc31f21f1ee1110f9158
gamee <gamee@EMRE>
2025-10-01 00:14:26 +0300

    UNİTOFWORK YAPISI EKLENDİ

---
+++ Core/HotelReservationApi.Application/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using HotelReservationApi.Application.Repositories;
+using HotelReservationApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.UnitOfWork
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        public IReadRepository<T> readRepository<T>() where T : class,IBaseEntity,new();
+        public IWriteRepository<T> writeRepository<T>() where T : class,IBaseEntity,new();
+        Task<int> SaveAsync();
+        int Save();
+    }
+}
---
+++ Infrastructure/HotelReservationApi.Persistence/UnitOf/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using HotelReservationApi.Application.Repositories;
+using HotelReservationApi.Application.UnitOfWork;
+using HotelReservationApi.Persistence.ApplicationContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Persistence.UnitOf
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+           return  await _context.SaveChangesAsync();
+        }
+
+        IReadRepository<T> IUnitOfWork.readRepository<T>() => new Repositories.ReadRepository<T>(_context);
+
+        IWriteRepository<T> IUnitOfWork.writeRepository<T>() => new Repositories.WriteRepository<T>(_context);
+    }
+}
