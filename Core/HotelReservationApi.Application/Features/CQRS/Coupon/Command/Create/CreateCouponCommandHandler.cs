54153d36dff43aab87c32a2c105e68a9129b5a45
gamee <gamee@EMRE>
2025-10-03 23:58:04 +0300

    MEDİATR  UNİT OF WORK VE AUTOMAPPERLA PROJEDE CQRS İN OLUŞTURULMASINA BAŞLANDI

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Coupon/Command/Create/CreateCouponCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Coupon.Command.Create
+{
+    public class CreateCouponCommandHandler : IRequestHandler<CreateCouponCommandRequest>
+    {
+        private readonly IUnitOfWork _unitofWork;
+
+        public CreateCouponCommandHandler(IUnitOfWork unitofWork)
+        {
+            _unitofWork = unitofWork;
+        }
+
+        public Task Handle(CreateCouponCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
