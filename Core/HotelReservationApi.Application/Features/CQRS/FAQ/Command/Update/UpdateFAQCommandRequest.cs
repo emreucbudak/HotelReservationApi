b57d2fad37915aad5854dd264bed724b61cd3671
gamee <gamee@EMRE>
2025-10-05 23:45:14 +0300

    UPDATEFAQCOMMAND EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/FAQ/Command/Update/UpdateFAQCommandRequest.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.FAQ.Command.Update
+{
+    public class UpdateFAQCommandRequest : IRequest
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+    }
+}
