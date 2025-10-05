6178cbfecfd24daf982339aee5455d6654414885
gamee <gamee@EMRE>
2025-10-05 14:43:39 +0300

    GETALL COUPONS EKLENDİ VE PROFİLES DÜZENLEMESİ YAPILDI

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Coupon/Queries/GetAll/GetAllCouponQueriesHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Coupon.Queries.GetAll
+{
+    public class GetAllCouponQueriesHandler : IRequestHandler<GetAllCouponQueriesRequest, List<GetAllCouponQueriesResponse>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mp;
+
+        public GetAllCouponQueriesHandler(IUnitOfWork unitOfWork, IMapper mp)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mp = mp;
+        }
+
+        public async Task<List<GetAllCouponQueriesResponse>> Handle(GetAllCouponQueriesRequest request, CancellationToken cancellationToken)
+        {
+            var coupons = await unitOfWork.readRepository<Domain.Entities.Coupon>().GetAllAsync(enableTracking: false);
+            var mapped = mp.Map<List<GetAllCouponQueriesResponse>>(coupons);
+            return mapped;
+        }
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/Coupon/Queries/GetAll/GetAllCouponQueriesResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Coupon.Queries.GetAll
+{
+    public class GetAllCouponQueriesResponse
+    {
+        public string CouponCode { get; set; }
+        public int DiscountPercentage { get; set; }
+        public int MaxUsageCount { get; set; }
+        public int CurrentUsageCount { get; set; }
+    }
+}
