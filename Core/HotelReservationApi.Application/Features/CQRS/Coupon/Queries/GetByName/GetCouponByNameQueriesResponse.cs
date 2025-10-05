0abe417bce8a582ceb36e650816e69ffc9f5a407
gamee <gamee@EMRE>
2025-10-05 14:59:03 +0300

    KULLANICI TARAFI İÇİN GETCOUPONBYNAME EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Coupon/Queries/GetByName/GetCouponByNameQueriesResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Coupon.Queries.GetByName
+{
+    public class GetCouponByNameQueriesResponse
+    {
+        public string CouponCode { get; set; }
+        public int DiscountPercentage { get; set; }
+        public int MaxUsageCount { get; set; }
+        public int CurrentUsageCount { get; set; }
+    }
+}
