94bfa96fc7524812e1afe2195e0ce7bf2048e8ff
gamee <gamee@EMRE>
2025-10-05 23:39:23 +0300

    DELETEFAQCOMMAND EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/FAQ/Command/Delete/DeleteFAQCommandRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.FAQ.Command.Delete
+{
+    public class DeleteFAQCommandRequest : IRequest
+    {
+        public int Id   { get; set; }
+    }
+}
