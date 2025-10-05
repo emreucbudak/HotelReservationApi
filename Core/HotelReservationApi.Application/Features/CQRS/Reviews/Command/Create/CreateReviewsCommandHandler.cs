fc3ccc22a498c523451b2d0e979abaf0a71ec2ec
gamee <gamee@EMRE>
2025-10-06 00:23:30 +0300

    CREATEREVİEWS EKLENDİ VE PROFİLE AYARLARI YAPILDI

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Reviews/Command/Create/CreateReviewsCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Reviews.Command.Create
+{
+    public class CreateReviewsCommandHandler : IRequestHandler<CreateReviewsCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mp;
+
+        public CreateReviewsCommandHandler(IUnitOfWork unitOfWork, IMapper mp)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mp = mp;
+        }
+
+        public async Task Handle(CreateReviewsCommandRequest request, CancellationToken cancellationToken)
+        {
+            var review = mp.Map<Domain.Entities.Reviews>(request);
+            await unitOfWork.writeRepository<Domain.Entities.Reviews>().AddAsync(review);
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
