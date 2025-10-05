b5ba6a8c46bfdefeeb90a534d1ee5147bb67e92d
gamee <gamee@EMRE>
2025-10-05 23:34:46 +0300

    CREATEFAQCOMMAND EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/FAQ/Command/Create/CreateFAQCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HotelReservationApi.Application.UnitOfWork;
+using HotelReservationApi.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.FAQ.Command.Create
+{
+    public class CreateFAQCommandHandler : IRequestHandler<CreateFAQCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mp;
+
+        public CreateFAQCommandHandler(IUnitOfWork unitOfWork, IMapper mp)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mp = mp;
+        }
+
+        public async Task Handle(CreateFAQCommandRequest request, CancellationToken cancellationToken)
+        {
+            var mapped = mp.Map<HotelReservationApi.Domain.Entities.FAQ>(request);
+            await unitOfWork.writeRepository<HotelReservationApi.Domain.Entities.FAQ>().AddAsync(mapped);
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/FAQ/Command/Create/CreateFAQCommandRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.FAQ.Command.Create
+{
+    public class CreateFAQCommandRequest : IRequest
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int HotelID { get; set; }
+    }
+}
