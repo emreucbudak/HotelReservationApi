f1a4f7b73b1d0dd15273b8fc5a2a32468fb4f628
gamee <gamee@EMRE>
2025-10-06 00:39:44 +0300

    100 COMMİTE ULASILDI DARISI 100 BRANCHE  OTEL IDSINE GÖRE YORUMLARA ULASMA EKLENDİ PROFİL AYARI YAPILDI

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Reviews/Queries/GetByHotelId/GetReviewsByHotelIdQueriesRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Reviews.Queries.GetByHotelId
+{
+    public class GetReviewsByHotelIdQueriesRequest : IRequest<List<GetReviewsByHotelIdQueriesResponse>>
+    {
+
+        public int HotelsId { get; set; }
+    }
+}
