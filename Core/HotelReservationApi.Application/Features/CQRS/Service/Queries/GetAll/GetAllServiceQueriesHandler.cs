06b7ffe7cac7cacaf66405de1a2691e4f42f62d3
gamee <gamee@EMRE>
2025-10-06 00:19:11 +0300

    getallservicequeries eklendi!

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Service/Queries/GetAll/GetAllServiceQueriesHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Service.Queries.GetAll
+{
+    public class GetAllServiceQueriesHandler : IRequestHandler<GetAllServiceQueriesRequest, List<GetAllServiceQueriesResponse>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mp;
+
+        public GetAllServiceQueriesHandler(IUnitOfWork unitOfWork, IMapper mp)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mp = mp;
+        }
+
+        public async Task<List<GetAllServiceQueriesResponse>> Handle(GetAllServiceQueriesRequest request, CancellationToken cancellationToken)
+        {
+           var service = await  unitOfWork.readRepository<Domain.Entities.Service>().GetAllAsync(predicate: x => x.HotelsId == request.Id, enableTracking: false);
+            var services = mp.Map<List<GetAllServiceQueriesResponse>>(service);
+            return services;
+        }
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/Service/Queries/GetAll/GetAllServiceQueriesResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Service.Queries.GetAll
+{
+    public class GetAllServiceQueriesResponse
+    {
+        public string ServiceName { get; set; }
+        public bool IsNeedAFee { get; set; }
+    }
+}
