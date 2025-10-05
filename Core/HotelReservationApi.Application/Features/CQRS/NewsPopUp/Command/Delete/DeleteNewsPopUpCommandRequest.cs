08b8fa92603d84c74184c26f6034523b96dc55d2
gamee <gamee@EMRE>
2025-10-05 20:52:51 +0300

    DELETENEWSPOPUPCOMMAND EKLENDİİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/NewsPopUp/Command/Delete/DeleteNewsPopUpCommandRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.NewsPopUp.Command.Delete
+{
+    public class DeleteNewsPopUpCommandRequest : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
