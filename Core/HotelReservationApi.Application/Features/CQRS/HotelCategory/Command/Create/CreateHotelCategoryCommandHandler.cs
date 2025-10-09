951f0ac3b8d158b496342ed0d5acf8c7145135ff
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-09 20:59:06 +0300

    CREATEHOTELCATEGORYCOMMAND EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelCategory/Command/Create/CreateHotelCategoryCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelCategory.Command.Create
+{
+    public class CreateHotelCategoryCommandHandler : IRequestHandler<CreateHotelCategoryCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CreateHotelCategoryCommandHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(CreateHotelCategoryCommandRequest request, CancellationToken cancellationToken)
+        {
+            var newHotelCategory = new Domain.Entities.HotelCategory
+            {
+                HotelCategoryName = request.HotelCategoryName
+            };
+            await unitOfWork.writeRepository<HotelReservationApi.Domain.Entities.HotelCategory>().AddAsync(newHotelCategory);
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelCategory/Command/Create/CreateHotelCategoryCommandRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelCategory.Command.Create
+{
+    public class CreateHotelCategoryCommandRequest : IRequest
+    {
+        public string HotelCategoryName { get; set; }
+    }
+}
