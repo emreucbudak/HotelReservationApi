89f6e21c7e4ab2b069d7bddf605886880de769a9
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-09 23:38:02 +0300

    deletehotelpoliticy command eklendi

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelsPoliticy/Command/Delete/DeleteHotelsPoliticyCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelsPoliticy.Command.Delete
+{
+    public class DeleteHotelsPoliticyCommandHandler : IRequestHandler<DeleteHotelsPoliticyCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public DeleteHotelsPoliticyCommandHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteHotelsPoliticyCommandRequest request, CancellationToken cancellationToken)
+        {
+            var hotelsPoliticy = await  unitOfWork.readRepository<HotelReservationApi.Domain.Entities.HotelsPoliticy>().GetByExpression(predicate: x => x.Id == request.Id, enableTracking: false);
+            await unitOfWork.writeRepository<HotelReservationApi.Domain.Entities.HotelsPoliticy>().DeleteAsync(hotelsPoliticy);
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
