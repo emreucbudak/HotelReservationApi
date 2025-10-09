f0bd8f36575f4cc6909bea95566b5253cbeb7b32
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-09 23:29:33 +0300

    createhotelspoliticycommand eklendi automapper ayarı yapıldı

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelsPoliticy/Command/Create/CreateHotelsPoliticyCommandRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelsPoliticy.Command.Create
+{
+    public class CreateHotelsPoliticyCommandRequest : IRequest
+    {
+        public string PoliticyName { get; set; }
+        public string PoliticyDescription { get; set; }
+        public int HotelId { get; set; }
+    }
+}
