227b3e5b2cf214643e20c0db1f1e8403c0db52e9
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-10 18:38:23 +0300

    PROFİLES AYARI VE CREATE Bills command eklendi

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Bills/Command/Create/CreateBillsCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Bills.Command.Create
+{
+    public class CreateBillsCommandHandler : IRequestHandler<CreateBillsCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public CreateBillsCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public async Task Handle(CreateBillsCommandRequest request, CancellationToken cancellationToken)
+        {
+            var newBill = mapper.Map<Domain.Entities.Bills>(request);   
+            await unitOfWork.writeRepository<Domain.Entities.Bills>().AddAsync(newBill);
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/Bills/Command/Create/CreateBillsCommandRequest.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Bills.Command.Create
+{
+    public class CreateBillsCommandRequest : IRequest
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int PaymetMethodId { get; set; }
+        public int PaymentTimingId { get; set; }
+        public int ReservationId { get; set; }
+        public int Price { get; set; }
+    }
+}
