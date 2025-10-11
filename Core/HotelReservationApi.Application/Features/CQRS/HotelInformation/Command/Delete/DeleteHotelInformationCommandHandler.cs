d9ce59a7f316170cd1cdad3c521060c45b40ed67
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-11 19:42:06 +0300

    DELETEHOTELINFORMATİON COMMAND EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelInformation/Command/Delete/DeleteHotelInformationCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelInformation.Command.Delete
+{
+    public class DeleteHotelInformationCommandHandler : IRequestHandler<DeleteHotelInformationCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public DeleteHotelInformationCommandHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteHotelInformationCommandRequest request, CancellationToken cancellationToken)
+        {
+            var hotelInformation = await unitOfWork.readRepository<Domain.Entities.HotelInformation>().GetByExpression(enableTracking: false, predicate: x => x.Id == request.HotelInformationId);
+            await unitOfWork.writeRepository<Domain.Entities.HotelInformation>().DeleteAsync(hotelInformation);
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
