bf54258fecc4f8d22369840fb13863320b837b83
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-11 23:29:27 +0300

    DELETEHOTELSSERVİCECOMMANDI EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelsService/Command/Delete/DeleteHotelsServiceCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelsService.Command.Delete
+{
+    public class DeleteHotelsServiceCommandHandler : IRequestHandler<DeleteHotelsServiceCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public DeleteHotelsServiceCommandHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteHotelsServiceCommandRequest request, CancellationToken cancellationToken)
+        {
+           var hotelsService =  await unitOfWork.readRepository<Domain.Entities.HotelServices>().GetByExpression(enableTracking: false, predicate: x => x.Id == request.Id);
+             await unitOfWork.writeRepository<Domain.Entities.HotelServices>().DeleteAsync(hotelsService);
+              await unitOfWork.SaveAsync();
+        }
+
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelsService/Command/Delete/DeleteHotelsServiceCommandRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelsService.Command.Delete
+{
+    public class DeleteHotelsServiceCommandRequest : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
