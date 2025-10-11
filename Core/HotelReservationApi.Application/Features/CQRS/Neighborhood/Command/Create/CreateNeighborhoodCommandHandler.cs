0aea07adf62d2aebe68e86dc3a53c4d000bd44f2
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-11 23:50:02 +0300

    CREATENEİGHBORHOODCOMMANDI EKLEDİM

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Neighborhood/Command/Create/CreateNeighborhoodCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Neighborhood.Command.Create
+{
+    public class CreateNeighborhoodCommandHandler : IRequestHandler<CreateNeighborhoodCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CreateNeighborhoodCommandHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(CreateNeighborhoodCommandRequest request, CancellationToken cancellationToken)
+        {
+            var neighborhood = new Domain.Entities.Neighborhood
+            {
+                Name = request.Name,
+                DistrictId = request.DistrictId
+            };
+            await unitOfWork.writeRepository<Domain.Entities.Neighborhood>().AddAsync(neighborhood);
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
