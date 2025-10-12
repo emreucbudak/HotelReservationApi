313af3848f6fcd5a001eb60a194a7b0df2fe7d23
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-13 00:56:07 +0300

    ADSBANNERNOTFOUND EXCEPTİONS EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/AdsBanner/Exceptions/AdsBannerNotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.AdsBanner.Exceptions
+{
+    public class AdsBannerNotFoundExceptions : NotFoundExceptions
+    {
+        public AdsBannerNotFoundExceptions(int id) : base($"{id}'e sahip ads reklam bannerı bulunamadı!")
+        {
+        }
+    }   
+    }
