558bf5491cffd433e4b4eeabe6dbbd9a293d6ceb
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-13 13:38:29 +0300

    HOWFARSPECİAL PLACE HATA YÖNETİM EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HowFarSpecialPlace/Exceptions/HowFarSpecialGetByIdNotFoundExceptions.cs
@@ -0,0 +1,18 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HowFarSpecialPlace.Exceptions
+{
+    public class HowFarSpecialGetByIdNotFoundExceptions : NotFoundExceptions
+    {
+        public HowFarSpecialGetByIdNotFoundExceptions(int id) : base($"{id}'e  sahip hotele ait  önemli konum bulunamadı!")
+        {
+        }
+    }
+    {
+    }
+}
