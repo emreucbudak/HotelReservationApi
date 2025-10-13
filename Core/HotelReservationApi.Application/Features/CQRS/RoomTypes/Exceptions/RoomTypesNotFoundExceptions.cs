afd4eededc8adaf9a77ca49c74544b9b38ece619
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-13 23:47:02 +0300

    ROOMTYPES ÖZELLEŞTİRİLMİŞ HATASI OLUŞTURULDU VE DELETEROOMTYPESA UYGULANDI

---
+++ Core/HotelReservationApi.Application/Features/CQRS/RoomTypes/Exceptions/RoomTypesNotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.RoomTypes.Exceptions
+{
+    public class RoomTypesNotFoundExceptions : NotFoundExceptions
+    {
+        public RoomTypesNotFoundExceptions(int id) : base($"{id}'e sahip  oda tipi bulunamadı!")
+        {
+        }
+    }
+}
