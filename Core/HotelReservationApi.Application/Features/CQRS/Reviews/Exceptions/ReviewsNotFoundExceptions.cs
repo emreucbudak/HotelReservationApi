9e43d2be7675a05d4160edcf846cce49ece7c1c2
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-13 23:58:10 +0300

    REVİEWSNOTFOUNDEXCEPTİONS ÖZELLEŞTİİRİLMİŞ HATASI EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Reviews/Exceptions/ReviewsNotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Reviews.Exceptions
+{
+    public class ReviewsNotFoundExceptions : NotFoundExceptions
+    {
+        public ReviewsNotFoundExceptions(int id) : base($"{id}'e sahip yorum bulunamadı")
+        {
+        }
+    }
+}
