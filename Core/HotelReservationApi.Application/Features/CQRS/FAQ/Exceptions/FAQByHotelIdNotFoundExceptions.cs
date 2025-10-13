46e8af5f558a32a16ddaaa904eb7ad7e8a09ab64
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-13 12:44:44 +0300

    FAQByHotelId not found exceptions eklenip delete ve updatefaq ında not foundexceptions uygulaması yapıldı

---
+++ Core/HotelReservationApi.Application/Features/CQRS/FAQ/Exceptions/FAQByHotelIdNotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.FAQ.Exceptions
+{
+    public class FAQByHotelIdNotFoundExceptions : NotFoundExceptions
+    {
+        public FAQByHotelIdNotFoundExceptions(int id) : base($"{id}'e otelinize ait SSS bulunamadı")
+        {
+        }
+    }
+}
