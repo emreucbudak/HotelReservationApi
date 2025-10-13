b4168a5c7c82d32d99d5ae59956b258713699561
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-13 13:07:28 +0300

    OTEL IDYE GÖRE VE NORMAL IDE GÖRE HOTELIINFORMATİON NOTFOUNDEXCEPTİONS EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelInformation/Exceptions/HotelInformationNotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelInformation.Exceptions
+{
+    public class HotelInformationNotFoundExceptions : NotFoundExceptions
+    {
+        public HotelInformationNotFoundExceptions(int id) : base($"{id}'e sahip hotel bilgisi bulunamadı!")
+        {
+        }
+    }
+}
