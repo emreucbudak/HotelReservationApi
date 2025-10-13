8e649f33edb9341e4188326700f35ad4a82b3ca6
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-13 13:29:52 +0300

    HOTELSSERVİCE ÖZELLEŞTİRİLMİŞ HATALARI EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelsService/Exceptions/HotelsServiceByIdNotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelsService.Exceptions
+{
+    public class HotelsServiceByIdNotFoundExceptions : NotFoundExceptions
+    {
+        public HotelsServiceByIdNotFoundExceptions(int id) : base($"{id}'e sahip hotelin servisleri bulunamadı!")
+        {
+        }
+    }
+}
