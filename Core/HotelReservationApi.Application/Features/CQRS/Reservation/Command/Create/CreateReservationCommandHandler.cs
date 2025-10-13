247b122d1161158d4e2481bd441a122f3bd6103d
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-14 00:23:49 +0300

    CREATERESERVATİON COMMAND EKLENDİ

--- Core/HotelReservationApi.Application/Features/CQRS/Reservation/Command/Create/CreateReservationCommandHandler.cs
+++ Core/HotelReservationApi.Application/Features/CQRS/Reservation/Command/Create/CreateReservationCommandHandler.cs
@@ -18,11 +18,31 @@ namespace HotelReservationApi.Application.Features.CQRS.Reservation.Command.Crea
             this.unitOfWork = unitOfWork;
         }
 
-        public Task Handle(CreateReservationCommandRequest request, CancellationToken cancellationToken)
+        public  async Task Handle(CreateReservationCommandRequest request, CancellationToken cancellationToken)
         {
             List<HotelReservationApi.Domain.Entities.Customer> customers = new();
             List<HotelReservationApi.Domain.Entities.Rooms> rooms = new();
-            foreach
+            foreach (int customerId in request.Customer)
+            {
+                var customer = unitOfWork.readRepository<HotelReservationApi.Domain.Entities.Customer>().GetByExpression(predicate: x => x.Id == customerId, enableTracking: false).Result;
+                customers.Add(customer);
+   
+            }
+            foreach (int roomId in request.Rooms)
+            {
+                var room = unitOfWork.readRepository<HotelReservationApi.Domain.Entities.Rooms>().GetByExpression(predicate: x => x.Id == roomId, enableTracking: false).Result;
+                rooms.Add(room);
+            }
+            Domain.Entities.Reservation reservation = new Domain.Entities.Reservation
+            {
+                Rooms = rooms,
+                Customer = customers,
+                StartDate = request.StartDate,
+                EndDate = request.EndDate
+
+            };
+            await unitOfWork.writeRepository<Domain.Entities.Reservation>().AddAsync(reservation);
+            await unitOfWork.SaveAsync();
         }
     }
 }
