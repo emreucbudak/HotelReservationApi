1514111813f286bf1f482dd9db257395ff2fbc28
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-13 13:14:08 +0300

    HOTELSPOLİTİCYNİN HATALARI EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelsPoliticy/Exceptions/HotelsPoliticyByIdNotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelsPoliticy.Exceptions
+{
+    public class HotelsPoliticyByIdNotFoundExceptions : NotFoundExceptions
+    {
+        public HotelsPoliticyByIdNotFoundExceptions(int id) : base($"{id}'e sahip hotelin politikaları bulunamadı")
+        {
+        }
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelsPoliticy/Exceptions/HotelsPoliticyNotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelsPoliticy.Exceptions
+{
+    public class HotelsPoliticyNotFoundExceptions : NotFoundExceptions
+    {
+        public HotelsPoliticyNotFoundExceptions(int id) : base($"{id}'e sahip hotel politakası bulunamadı!")
+        {
+        }
+    }
+}
