d298c409a3cbff45c5d6f553eabf1b075a777b10
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-13 12:53:29 +0300

    hotelcategorynotfoundexceptions eklendi

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelCategory/Exceptions/HotelCategoryNotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelCategory.Exceptions
+{
+    public class HotelCategoryNotFoundExceptions : NotFoundExceptions
+    {
+        public HotelCategoryNotFoundExceptions(int id) : base($"{id}'e sahip hotelcategory eklendi")
+        {
+        }
+    }
+}
