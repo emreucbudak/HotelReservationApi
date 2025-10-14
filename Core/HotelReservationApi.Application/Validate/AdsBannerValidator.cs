158a5f477da43d6c42188e633ca553e59fe1d520
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-14 12:54:11 +0300

    FLUENT VALİDATİON VE ADSBANNERVALİDATOR EKLENDİ

---
+++ Core/HotelReservationApi.Application/Validate/AdsBannerValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using HotelReservationApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Validate
+{
+    public class AdsBannerValidator : AbstractValidator<AdsBanner>
+    {
+        public AdsBannerValidator()
+        {
+            RuleFor(x=> x.Title).MinimumLength(20).MaximumLength(50).WithMessage("Başlık 20 ile 50 karakter arasında olmalı").NotEmpty().WithMessage("Başlık boş olamaz");
+            RuleFor(x=> x.Description).MinimumLength(50).MaximumLength(500).WithMessage("Açıklama 50 ile 500 karakter arasında olmalı").NotEmpty().WithMessage("Açıklama boş olamaz");
+        }
+    }
+}
--- Presentation/HotelReservationApi.Presentation/Program.cs
+++ Presentation/HotelReservationApi.Presentation/Program.cs
@@ -1,6 +1,7 @@
 using HotelReservationApi.Application.AutoMapper;
 using HotelReservationApi.Application.Repositories;
 using HotelReservationApi.Application.UnitOfWork;
+using HotelReservationApi.Domain.Entities;
 using HotelReservationApi.Persistence.ApplicationContext;
 using HotelReservationApi.Persistence.Repositories;
 using HotelReservationApi.Persistence.UnitOf;
@@ -8,6 +9,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.EntityFrameworkCore;
 using System;
+using FluentValidation;
+using HotelReservationApi.Application.Validate;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +28,8 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
 builder.Services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddValidatorsFromAssemblyContaining<AdsBannerValidator>();
+
 
 
 var app = builder.Build();
