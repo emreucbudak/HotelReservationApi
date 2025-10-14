c7f136e9ec58b49e3c37f7236f46cf855e5d2b18
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-14 20:12:09 +0300

    HOWFARSPECİALPLACEVALİDATOR EKLENDİ

---
+++ Core/HotelReservationApi.Application/Validate/HowFarSpecialPlaceValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Validate
+{
+    public class HowFarSpecialPlaceValidator : AbstractValidator<Domain.Entities.HowFarSpecialPlace>
+    {
+        public HowFarSpecialPlaceValidator()
+        {
+            RuleFor(x => x.PlaceName).NotEmpty().WithMessage("Yer adı boş olamaz!").MinimumLength(7).WithMessage("Yer adı en az 7 karakter olabilir!").MaximumLength(100).WithMessage("Yer adı en fazla 100 karakter olabilir!");
+            RuleFor(x => x.SpecialPlaceCategoryId).NotEmpty().WithMessage("Özel yer kategorisi boş olamaz!");
+            RuleFor(x => x.Distance).NotEmpty().WithMessage("Mesafe boş olamaz!").GreaterThan(0).WithMessage("Mesafe sıfırdan büyük olmalıdır!");
+            RuleFor(x => x.HotelsId).NotEmpty().WithMessage("Otel bilgisi boş olamaz!");
+        }
+    }
+}
