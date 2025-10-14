8c9c0031cc03ba3c9883f6c09d4d6a8b79c13733
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-14 20:19:35 +0300

    NEWSPOPUPVALİDATOR EKLENDİ

--- Core/HotelReservationApi.Application/Validate/AdsBannerValidator.cs
+++ Core/HotelReservationApi.Application/Validate/AdsBannerValidator.cs
@@ -14,6 +14,7 @@ namespace HotelReservationApi.Application.Validate
         {
             RuleFor(x=> x.Title).MinimumLength(20).MaximumLength(50).WithMessage("Başlık 20 ile 50 karakter arasında olmalı").NotEmpty().WithMessage("Başlık boş olamaz");
             RuleFor(x=> x.Description).MinimumLength(50).MaximumLength(500).WithMessage("Açıklama 50 ile 500 karakter arasında olmalı").NotEmpty().WithMessage("Açıklama boş olamaz");
+            RuleFor(x=> x.ImageUrl).NotEmpty().WithMessage("Resim kaynağı boş olamaz");
         }
     }
 }
