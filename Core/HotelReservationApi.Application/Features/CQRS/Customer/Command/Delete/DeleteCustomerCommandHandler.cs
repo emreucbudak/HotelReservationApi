17a9d851d6ffcff7156a607784831ee4a5059dfc
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-14 11:42:55 +0300

    CUSTOMERNOTFOUND EXCEPTİON EKLENDİ UPDATE VE DELETE CUSTOMER COMMANDA HATA YÖNETİMİ EKLENDİ

--- Core/HotelReservationApi.Application/Features/CQRS/Customer/Command/Delete/DeleteCustomerCommandHandler.cs
+++ Core/HotelReservationApi.Application/Features/CQRS/Customer/Command/Delete/DeleteCustomerCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using HotelReservationApi.Application.UnitOfWork;
+﻿using HotelReservationApi.Application.Features.CQRS.Customer.Exceptions;
+using HotelReservationApi.Application.UnitOfWork;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,10 @@ namespace HotelReservationApi.Application.Features.CQRS.Customer.Command.Delete
         public async Task Handle(DeleteCustomerCommandRequest request, CancellationToken cancellationToken)
         {
             var customer = await unitOfWork.readRepository<Domain.Entities.Customer>().GetByExpression(predicate: x=> x.Id == request.Id,enableTracking:false);
+            if (customer is null)
+            {
+                throw new CustomerNotFoundExceptions(request.Id);
+            }
             await unitOfWork.writeRepository<Domain.Entities.Customer>().DeleteAsync(customer);
             await unitOfWork.SaveAsync();
         }
