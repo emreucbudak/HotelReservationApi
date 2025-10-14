bc6374a38b5a53fcedb85abb2f81b9489dd5d91f
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-14 21:12:56 +0300

    SERVİCEVALİDATOR EKLENDİ

---
+++ Core/HotelReservationApi.Application/Validate/ServiceValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Validate
+{
+    public class ServiceValidator : AbstractValidator<Domain.Entities.Service>
+    {
+        public ServiceValidator()
+        {
+            RuleFor(s => s.ServiceName)
+                .NotEmpty().WithMessage("Hizmet adı boş olamaz")
+                .MinimumLength(5).WithMessage("Hizmet adı en az 5 karakter olabilir!")
+                .MaximumLength(50).WithMessage("Hizmet adı en fazla 50 karakter olabilir!");
+        }
+    }
+}
