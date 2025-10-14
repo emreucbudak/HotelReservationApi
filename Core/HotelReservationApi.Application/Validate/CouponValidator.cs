427a660932cc56611cd5c35f2b554c5126168940
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-14 15:12:02 +0300

    KUPON VALİDATORU EKLENDİ

---
+++ Core/HotelReservationApi.Application/Validate/CouponValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Validate
+{
+    public class CouponValidator : AbstractValidator<Domain.Entities.Coupon>
+    {
+        public CouponValidator()
+        {
+            RuleFor(x => x.CouponCode)
+                .NotEmpty().WithMessage("Kupon kodu boş olamaz!")
+                .MinimumLength(5).WithMessage("Kupon kodu en az 5 karakter olmalıdır!")
+                .MaximumLength(12).WithMessage("Kupon kodu en fazla 12 karakter olmalıdır!").NotNull().NotEmpty().WithMessage("Kupon kodu boş veya null olamaz!");
+            RuleFor(x => x.DiscountPercentage).GreaterThan(0).WithMessage("İndirim yüzdesi 0'dan büyük olmalıdır!").NotEmpty().WithMessage("İndirim oranı boş olamaz");
+            RuleFor(x => x.MaxUsageCount).GreaterThan(0).WithMessage("Maksimum kullanım sayısı 0'dan büyük olmalıdır!").NotEmpty().WithMessage("Maksimum kullanım sayısı boş olamaz");
+            RuleFor(x => x.CurrentUsageCount).GreaterThanOrEqualTo(0).WithMessage("Mevcut kullanım sayısı 0 veya daha büyük olmalıdır!").NotEmpty().WithMessage("Mevcut kullanım sayısı boş olamaz");
+        }
+    }
+}
