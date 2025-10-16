3dd266ecbe8dd8affde3c4d144cfff282b53e7a9
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-17 01:20:56 +0300

    AUTHEXCEPTİONHANDLER EKLENDİ

---
+++ Presentation/HotelReservationApi.Presentation/ExceptionHandler/AuthExceptionHandler.cs
@@ -0,0 +1,39 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace HotelReservationApi.Presentation.ExceptionHandler
+{
+    public sealed class AuthExceptionHandler : IExceptionHandler
+    {
+        private readonly IProblemDetailsService _prds;
+
+        private readonly ILogger<AuthException> _log;
+        public AuthExceptionHandler(IProblemDetailsService prds, ILogger<AuthException> log)
+        {
+            _prds = prds;
+            _log = log;
+        }
+
+
+        public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            if (exception is not AuthException authException)
+            {
+                return ValueTask.FromResult(false);
+            }
+            _log.LogWarning("Bir hata oluştu! Hata : {Error}", exception.Message);
+            var context = new ProblemDetailsContext()
+            {
+                HttpContext = httpContext,
+                Exception = exception,
+                ProblemDetails = new()
+                {
+                    Title = "Bir hata oluştu",
+                    Detail = exception.Message,
+                    Status = StatusCodes.Status401Unauthorized,
+                }
+            };
+            return _prds.TryWriteAsync(context);
+        }
+    }
+}
