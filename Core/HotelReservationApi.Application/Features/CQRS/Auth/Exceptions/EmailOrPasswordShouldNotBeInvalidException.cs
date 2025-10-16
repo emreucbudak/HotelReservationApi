c080f4bf361e628e7a962346b998c442615ad767
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-17 01:35:47 +0300

    GİRİŞ BİLGİSİ HATASI VE RESFRESH TOKEN SÜRESİ HATASI EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Auth/Exceptions/EmailOrPasswordShouldNotBeInvalidException.cs
@@ -0,0 +1,15 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Auth.Exceptions
+{
+    public class EmailOrPasswordShouldNotBeInvalidException : AuthException
+    {
+        public EmailOrPasswordShouldNotBeInvalidException() : base("Kullanıcı adı veya şifre yanlıştır.") { }
+
+    }
+}
