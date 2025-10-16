e8a218294186cf344eb014382419f0f7ee4da434
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-16 21:12:23 +0300

    HOTELMANAGER VE RECEPTİON MODELLERİ EKLENDİ

--- Core/HotelReservationApi.Domain/Entities/Reception.cs
+++ Core/HotelReservationApi.Domain/Entities/Reception.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelReservationApi.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace HotelReservationApi.Domain.Entities
 {
-    internal class Reception
-    {
+    public class Reception : BaseEntity
+    { 
+        public int HotelsId { get; set; }
+        public Hotels Hotels { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
