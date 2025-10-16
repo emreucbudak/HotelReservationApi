6a79d8ebc239fc4923e76d6e38a1cbf4c952e01a
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-17 01:37:35 +0300

    KULLANICI ZATEN VAR HATASI EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Auth/Exceptions/UserAlreadyExistException.cs
@@ -0,0 +1,14 @@
+﻿using HotelReservationApi.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Auth.Exceptions
+{
+    public class UserAlreadyExistException : AuthException
+    {
+        public UserAlreadyExistException() : base("Böyle bir kullanıcı zaten var!") { }
+    }
+}
