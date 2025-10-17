c3c183d8f4a56911bf9338c4f2b2836f7d7745f7
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-17 21:04:57 +0300

    HOTEL İMAGESİN EKSİK OLAN QUERYLERİ EKLENİP AUTOMAPPER AYARLARI YAPILDI

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelImages/Queries/GetAllByHotelId/GetAllHotelImagesByIdQueriesHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelImages.Queries.GetAllByHotelId
+{
+    public class GetAllHotelImagesByIdQueriesHandler : IRequestHandler<GetAllHotelImagesByIdQueriesRequest, List<GetAllHotelImagesByIdQueriesResponse>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper map;
+
+        public GetAllHotelImagesByIdQueriesHandler(IUnitOfWork unitOfWork, IMapper map)
+        {
+            _unitOfWork = unitOfWork;
+            this.map = map;
+        }
+
+        public async Task<List<GetAllHotelImagesByIdQueriesResponse>> Handle(GetAllHotelImagesByIdQueriesRequest request, CancellationToken cancellationToken)
+        {
+            var getAllHotelImages = await _unitOfWork.readRepository<Domain.Entities.HotelImages>().GetAllAsync(predicate: x=> x.Id == request.HotelId,enableTracking:false);
+            return map.Map<List<GetAllHotelImagesByIdQueriesResponse>>(getAllHotelImages);
+        }
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelImages/Queries/GetAllByHotelId/GetAllHotelImagesByIdQueriesRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelImages.Queries.GetAllByHotelId
+{
+    public class GetAllHotelImagesByIdQueriesRequest : IRequest<List<GetAllHotelImagesByIdQueriesResponse>>
+    {
+        public int HotelId { get; set; }
+    }
+}
