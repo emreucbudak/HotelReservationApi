86b5d1104641877c3fcda96e429496502fe8de07
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-17 15:45:13 +0300

    ROOMSCONTROLLER İÇİ DÜZELTİLDİ

--- Core/HotelReservationApi.Application/Features/CQRS/Rooms/Command/Delete/DeleteRoomsCommandRequest.cs
+++ Core/HotelReservationApi.Application/Features/CQRS/Rooms/Command/Delete/DeleteRoomsCommandRequest.cs
@@ -9,6 +9,11 @@ namespace HotelReservationApi.Application.Features.CQRS.Rooms.Command.Delete
 {
     public class DeleteRoomsCommandRequest : IRequest
     {
+        public DeleteRoomsCommandRequest(int id)
+        {
+            Id = id;
+        }
+
         public int Id { get; set; }
     }
 }
--- Core/HotelReservationApi.Application/Features/CQRS/Rooms/Queries/GetAll/GetAllRoomsQueriesRequest.cs
+++ Core/HotelReservationApi.Application/Features/CQRS/Rooms/Queries/GetAll/GetAllRoomsQueriesRequest.cs
@@ -9,8 +9,16 @@ namespace HotelReservationApi.Application.Features.CQRS.Rooms.Queries.GetAll
 {
     public class GetAllRoomsQueriesRequest : IRequest<List<GetAllRoomsQueriesResponse>>
     {
+        public GetAllRoomsQueriesRequest(int id, int? pageCount, int? pageSize)
+        {
+            HotelId = id;
+            Page = pageCount ?? 1;
+            Size = pageSize ?? 10;
+        }
+
         public int HotelId { get; set; }
-        public int Page { get; set; } = 1;
-        public int Size { get; set; } = 10;
+        public int Page { get; set; } 
+        public int Size { get; set; }
+
     }
 }
