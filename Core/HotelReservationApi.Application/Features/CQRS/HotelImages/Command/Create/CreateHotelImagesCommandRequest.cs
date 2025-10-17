5823f1535b73b90bc68660b3fbdbe85dc90f912c
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-17 20:53:44 +0300

    HOTELIMAGES IN CQRSİNDE COMMANDLAR EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelImages/Command/Create/CreateHotelImagesCommandRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelImages.Command.Create
+{
+    public class CreateHotelImagesCommandRequest : IRequest
+    {
+        public string ImageUrl { get; set; }
+        public int HotelId { get; set; }
+        public string? ImageTitle { get; set; }
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/HotelImages/Command/Delete/DeleteHotelImagesCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.HotelImages.Command.Delete
+{
+    public class DeleteHotelImagesCommandHandler : IRequestHandler<DeleteHotelImagesCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public DeleteHotelImagesCommandHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteHotelImagesCommandRequest request, CancellationToken cancellationToken)
+        {
+            var hotelImages = await unitOfWork.readRepository<HotelReservationApi.Domain.Entities.HotelImages>().GetByExpression(predicate:x=> x.Id == request.Id,enableTracking:false);
+            await unitOfWork.writeRepository<Domain.Entities.HotelImages>().DeleteAsync(hotelImages);
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
