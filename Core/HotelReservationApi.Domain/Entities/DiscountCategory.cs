b0e8e869a3742d8044a1ad3d14bfdd04cabba9bb
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-19 23:43:58 +0300

    DİSCOUNTLİST MODELİ GÜNCELLENİP DİSCOUNT CATEGORY EKLENİP CQRS ONA  GÖRE DÜZENLENDİ

---
+++ Core/HotelReservationApi.Domain/Entities/DiscountCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Domain.Entities
+{
+    public class DiscountCategory
+    {
+        public int Id { get; set; }
+        public int DiscountCategoryName { get; set; }
+    }
+}
--- Core/HotelReservationApi.Domain/Entities/DiscountList.cs
+++ Core/HotelReservationApi.Domain/Entities/DiscountList.cs
@@ -13,23 +13,29 @@ namespace HotelReservationApi.Domain.Entities
         {
         }
 
-        public DiscountList(bool ısDiscountActive, int discountPercentage, bool ısDiscountForReservationDate, DateTime? discountStartTime, DateTime? discountEndTime, DateTime? bookingStartDate, DateTime? bookingEndDate)
+        public DiscountList(int discountPercentage, DateTime? discountStartTime, DateTime? discountEndTime, DateTime? bookingStartDate, DateTime? bookingEndDate, bool ısGlobal, int? stayDays, int? payDays)
         {
-            IsDiscountActive = ısDiscountActive;
+
             DiscountPercentage = discountPercentage;
-            IsDiscountForReservationDate = ısDiscountForReservationDate;
+
             DiscountStartTime = discountStartTime;
             DiscountEndTime = discountEndTime;
             BookingStartDate = bookingStartDate;
             BookingEndDate = bookingEndDate;
+            IsGlobal = ısGlobal;
+            StayDays = stayDays;
+            PayDays = payDays;
         }
-
-        public bool IsDiscountActive { get; set; }
+        public int DiscountCategoryId { get; set; }
+        public DiscountCategory DiscountCategory { get; set; }
+   
         public int DiscountPercentage { get; set; }
-        public bool IsDiscountForReservationDate { get; set; }
+        public bool IsGlobal { get; set; }
         public DateTime? DiscountStartTime { get; set; }
         public DateTime? DiscountEndTime { get; set; }
         public DateTime? BookingStartDate { get; set; }
         public DateTime? BookingEndDate { get; set; }
+        public int? StayDays { get; set; } 
+        public int? PayDays { get; set; }
     }
 }
