fadb7bcb531ca1fa9d8924bb124888d2547f8349
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-20 20:32:54 +0300

    login command  eklendi

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Auth/Login/LoginCommandRequest.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Auth.Login
+{
+    public class LoginCommandRequest : IRequest<LoginCommandResponse>
+    {
+        [DefaultValue("emreucbudak63@gmail.com")]
+        public string Email { get; set; }
+        [DefaultValue("emreucbudak")]
+        public string Password { get; set; }
+    }
+}
---
+++ Core/HotelReservationApi.Application/Features/CQRS/Auth/Login/LoginCommandResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Auth.Login
+{
+    public class LoginCommandResponse
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime Expiration { get; set; }
+
+    }
+}
