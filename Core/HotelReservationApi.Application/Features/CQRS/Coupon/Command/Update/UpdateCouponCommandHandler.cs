8d992ac06d6e1e54c93a3143e42af832a3635394
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-20 12:54:12 +0300

    KUPON KONTROL VE GÜNC ELLEME İŞLEMİ EKLENDİ

--- Core/HotelReservationApi.Application/Features/CQRS/Coupon/Command/Update/UpdateCouponCommandHandler.cs
+++ Core/HotelReservationApi.Application/Features/CQRS/Coupon/Command/Update/UpdateCouponCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HotelReservationApi.Application.Features.CQRS.Coupon.Exceptions;
+using HotelReservationApi.Application.UnitOfWork;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,28 @@ using System.Threading.Tasks;
 
 namespace HotelReservationApi.Application.Features.CQRS.Coupon.Command.Update
 {
-    internal class UpdateCouponCommandHandler
+    public class UpdateCouponCommandHandler : IRequestHandler<UpdateCouponCommandRequest>
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateCouponCommandHandler(IUnitOfWork unitOfWork )
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(UpdateCouponCommandRequest request, CancellationToken cancellationToken)
+        {
+            var coupon = await _unitOfWork.readRepository<Domain.Entities.Coupon>().GetByExpression(enableTracking:false,predicate:x=> x.CouponCode == request.CouponCode);
+            if (coupon is null)
+            {
+                throw new CouponNotFoundExceptions(request.CouponCode);
+            }
+
+            coupon.CurrentUsageCount += coupon.CurrentUsageCount;
+            coupon.IsDeleted = coupon.CurrentUsageCount == coupon.MaxUsageCount ? true : false;
+            await _unitOfWork.writeRepository<Domain.Entities.Coupon>().UpdateAsync(coupon);
+            await _unitOfWork.SaveAsync();
+
+        }
     }
 }
