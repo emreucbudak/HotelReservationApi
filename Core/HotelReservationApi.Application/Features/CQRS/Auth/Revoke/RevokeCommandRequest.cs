cb129f9c6677644837612be3531e29bb1c637f96
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-20 23:25:57 +0300

    REVOKECOMMANDI EKLENDİ

---
+++ Core/HotelReservationApi.Application/Features/CQRS/Auth/Revoke/RevokeCommandRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Features.CQRS.Auth.Revoke
+{
+    public class RevokeCommandRequest : IRequest
+    {
+        public string Email { get; set; }
+    }
+}
