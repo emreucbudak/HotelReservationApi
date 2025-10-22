de868c5f2259c1e989d494ba512b8371138cfe1c
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-22 22:34:48 +0300

    GETALLREVİEWSQUERİESE CACHE EKLENDİ

--- Core/HotelReservationApi.Application/Features/CQRS/Reviews/Queries/GetAll/GetAllReviewsQueriesHandler.cs
+++ Core/HotelReservationApi.Application/Features/CQRS/Reviews/Queries/GetAll/GetAllReviewsQueriesHandler.cs
@@ -2,11 +2,13 @@
 using HotelReservationApi.Application.UnitOfWork;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HotelReservationApi.Application.Features.CQRS.Reviews.Queries.GetAll
@@ -15,18 +17,30 @@ namespace HotelReservationApi.Application.Features.CQRS.Reviews.Queries.GetAll
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper mapper;
+        private readonly IDistributedCache cache;
 
-        public GetAllReviewsQueriesHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        public GetAllReviewsQueriesHandler(IUnitOfWork unitOfWork, IMapper mapper, IDistributedCache cache)
         {
             _unitOfWork = unitOfWork;
             this.mapper = mapper;
+            this.cache = cache;
         }
 
         public async Task<List<GetAllReviewsQueriesResponse>> Handle(GetAllReviewsQueriesRequest request, CancellationToken cancellationToken)
         {
+            var cacheKey = "Get_All_Reviews";
+            var allReviews = await cache.GetStringAsync(cacheKey);
+            if (allReviews is not null)
+                return JsonSerializer.Deserialize<List<GetAllReviewsQueriesResponse>>(allReviews);
             var reviews = await _unitOfWork.readRepository<HotelReservationApi.Domain.Entities.Reviews>().GetAllAsync(enableTracking:false,includable:x=> x.Include(y=> y.Hotels));
-            var allReviews = mapper.Map<List<GetAllReviewsQueriesResponse>>(reviews);
-            return allReviews;
+            var getAllReviews = mapper.Map<List<GetAllReviewsQueriesResponse>>(reviews);
+            var options = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30),
+                SlidingExpiration = TimeSpan.FromMinutes(10),
+            };
+            await cache.SetStringAsync(cacheKey, JsonSerializer.Serialize(getAllReviews),options);
+            return getAllReviews;
             
         }
     }
