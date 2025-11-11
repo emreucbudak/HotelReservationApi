96087ec170a3b29dac4e1ff20e3bdd94e0e3d546
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-11-12 02:08:27 +0300

    stripe servisi eklendi

---
+++ Core/HotelReservationApi.Application/Payment/IStripeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.Payment
+{
+    public interface IStripeService
+    {
+        Task<string> CreatePayment (int price,string currency,string paymentType);
+    }
+}
