e5b9ad9a16954856c4ffda23a6389d6b25b8d509
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-11-26 02:59:22 +0300

    EMAİL SERVİCE EKLENDİ

--- Infrastructure/HotelReservationApi.Infrastructure/Emails/EmailService.cs
+++ Infrastructure/HotelReservationApi.Infrastructure/Emails/EmailService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +10,33 @@ namespace HotelReservationApi.Infrastructure.Emails
 {
     public class EmailService : IEmailService
     {
-        public Task SendEmail(string to, string subject, string body, int verificationCode = 0)
+        public async Task SendEmail(string to, string subject, string body, int? verificationCode)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var mailMessage = new MailMessage())
+                {
+                    mailMessage.To.Add(to);
+                    mailMessage.Subject = verificationCode is null ? subject : "Doğrulama Kodu";
+                    mailMessage.Body = verificationCode is null ? body : $"Doğrulama onay Kodunuz : {verificationCode} {DateTime.Now}";
+                    mailMessage.From = new MailAddress("hotelapideneme@gmail.com");
+
+                    using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtpClient.Credentials = new System.Net.NetworkCredential("hotelapideneme@gmail.com", "qbzyeaooxbuufhzi");
+                        smtpClient.EnableSsl = true;
+                        smtpClient.UseDefaultCredentials = false;
+                        await smtpClient.SendMailAsync(mailMessage);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new InvalidOperationException("Email gönderilirken bir hata oluştu.", ex);
+
+
+            }
         }
     }
 }
