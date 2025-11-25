3e51e3dc5c8d527310c05ae95757f76541483df4
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-11-26 02:31:38 +0300

    EMAİL SERVİCE EKLENDİ İÇİ DÜZENLENECEK

---
+++ Infrastructure/HotelReservationApi.Infrastructure/Emails/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using HotelReservationApi.Application.Emails;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Infrastructure.Emails
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendEmail(string to, string subject, string body, int verificationCode = 0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
