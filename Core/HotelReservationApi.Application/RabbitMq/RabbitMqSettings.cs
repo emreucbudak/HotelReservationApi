77eb442313e0c9b4969a9d684fe2845c00487014
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-11-26 21:44:41 +0300

    PROJEYE RABBİTMQ ENTEGRASYONU YAPILMAYA DEVAM EDİLİYOR

---
+++ Core/HotelReservationApi.Application/RabbitMq/RabbitMqSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Application.RabbitMq
+{
+    public class RabbitMqSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string UsernameFile { get; set; }
+        public string PasswordFile { get; set; }
+    }
+}
---
+++ Infrastructure/HotelReservationApi.Infrastructure/RabbitMq/RabbitMqProducer.cs
@@ -0,0 +1,17 @@
+﻿using HotelReservationApi.Application.RabbitMq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Infrastructure.RabbitMq
+{
+    public class RabbitMqProducer : IMessageQueueService
+    {
+        public Task PublishAsync<T>(string queueName, T message) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
