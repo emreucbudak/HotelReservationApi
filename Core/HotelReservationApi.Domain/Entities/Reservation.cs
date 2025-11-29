3d7fcf50d02fe239e654ab468d1dd2162d3c6202
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-11-29 03:53:02 +0300

    BİLLPDFMODEL EKLENDİ

--- Core/HotelReservationApi.Domain/Entities/Reservation.cs
+++ Core/HotelReservationApi.Domain/Entities/Reservation.cs
@@ -10,9 +10,8 @@ namespace HotelReservationApi.Domain.Entities
         {
         }
 
-        public Reservation(ICollection<Rooms> rooms, DateOnly startDate, DateOnly endDate, ICollection<Customer> customer, int hotelsId, int totalPrice, DateOnly reservationDate, int memberId)
+        public Reservation( DateOnly startDate, DateOnly endDate, ICollection<Customer> customer, int hotelsId, int totalPrice, DateOnly reservationDate, int memberId)
         {
-            Rooms = rooms;
             StartDate = startDate;
             EndDate = endDate;
             Customer = customer;
@@ -22,7 +21,6 @@ namespace HotelReservationApi.Domain.Entities
             MemberId = memberId;
         }
 
-        public ICollection<Rooms> Rooms { get; set; }
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
         public ICollection<Customer> Customer { get; set; }
---
+++ Infrastructure/HotelReservationApi.Infrastructure/PdfWriter/Model/BillPdfModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApi.Infrastructure.PdfWriter.Model
+{
+    public class BillPdfModel
+    {
+        public Guid BillNo  { get; set; }
+        public DateOnly BillCreateDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string PaymentMethod { get; set; }
+        public string PaymentTiming { get; set; }
+        public IList<string> PeopleBooked { get; set; }
+        public int TotalAmount { get; set; }
+        public int TotalNights { get; set; }
+        public string Name { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public string HotelName { get; set; }
+        public IList<string> RoomTypes { get; set; }
+
+
+
+    }
+}
